d35ab110eddbf301cb15c39731ba5568897e1280
Finn <152090293+F1nnSBK@users.noreply.github.com>
2024-11-23 08:09:42 +0100

    adding delete for logged in useres

--- components/DatabaseEntries.vue
+++ components/DatabaseEntries.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="flex max-h-svh w-full flex-col rounded-t-lg bg-slate-200 shadow-lg">
-    <div
-      class="border-b-2 border-zinc-700 p-4"
-      v-for="item in data"
-      :key="item.timestamp"
-    >
+  <div
+    class="m-auto mb-40 flex max-h-svh w-full flex-col rounded-t-lg bg-slate-200 shadow-lg sm:w-8/12 lg:w-6/12"
+  >
+    <div class="border-b-2 border-zinc-700 p-4" v-for="item in data" :key="item.id">
       <div>
         {{ item.timestamp }}
       </div>
@@ -17,7 +15,7 @@
         </div>
         <div>
           <Icon
-            v-on:click.prevent=""
+            v-on:click.prevent="() => deleteEntry(item.id)"
             name="carbon:trash-can"
             color="red"
             class="cursor-pointer text-2xl"
@@ -29,27 +27,28 @@
 </template>
 
 <script lang="ts" setup>
-import { collection, getDocs, Firestore, doc } from 'firebase/firestore';
+import { collection, getDocs, Firestore, doc, deleteDoc } from 'firebase/firestore';
 import { useNuxtApp } from '#app';
 import { onMounted } from 'vue';
 
 interface FormData {
+  id: string;
   timestamp: string;
   name: string;
   phone: string;
 }
 
 const data = ref<FormData[]>([]);
+const { $db } = useNuxtApp();
+const db = $db as Firestore;
 
 onMounted(async () => {
-  const { $db } = useNuxtApp();
-  const db = $db as Firestore;
-
   try {
     const querySnapshot = await getDocs(collection(db, 'Landjugend'));
     querySnapshot.forEach((doc) => {
       const dbData = doc.data();
       data.value.push({
+        id: doc.id,
         timestamp: convertTimestamp(dbData.createdAt.seconds),
         name: dbData.name,
         phone: dbData.phone,
@@ -70,6 +69,15 @@ function convertTimestamp(unix_timestamp: number): string {
     hour: '2-digit',
     minute: '2-digit',
   });
+}
+
+async function deleteEntry(id: string) {
+  try {
+    await deleteDoc(doc(db, 'Landjugend', id));
+    data.value = data.value.filter((item) => item.id !== id);
+  } catch (error) {
+    console.error('Fehler beim Löschen des Eintrags:', error);
+  }
 }
 </script>
 
