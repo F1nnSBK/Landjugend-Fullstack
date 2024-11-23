acc89be738b4ac9d0a4f13be1857d67e771b9562
Finn <152090293+F1nnSBK@users.noreply.github.com>
2024-11-23 07:39:49 +0100

    convert timestamp

--- components/DatabaseEntries.vue
+++ components/DatabaseEntries.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="flex flex-col w-full rounded-t-lg bg-slate-200 shadow-lg max-h-svh">
-    <div class="border-b-2 border-zinc-700 p-4" v-for="item in data"  :key="item.timestamp">
+  <div class="flex max-h-svh w-full flex-col rounded-t-lg bg-slate-200 shadow-lg">
+    <div
+      class="border-b-2 border-zinc-700 p-4"
+      v-for="item in data"
+      :key="item.timestamp"
+    >
       <div>
         {{ item.timestamp }}
       </div>
       <div>
         {{ item.name }}
       </div>
-      <div class="flex justify-between items-center">
+      <div class="flex items-center justify-between">
         <div>
           {{ item.phone }}
         </div>
@@ -30,7 +34,7 @@ import { useNuxtApp } from '#app';
 import { onMounted } from 'vue';
 
 interface FormData {
-  timestamp: number;
+  timestamp: string;
   name: string;
   phone: string;
 }
@@ -44,20 +48,29 @@ onMounted(async () => {
   try {
     const querySnapshot = await getDocs(collection(db, 'Landjugend'));
     querySnapshot.forEach((doc) => {
-      const dbData = doc.data()
+      const dbData = doc.data();
       data.value.push({
-        timestamp: dbData.createdAt ? dbData.createdAt.seconds * 1000 : 0,
+        timestamp: convertTimestamp(dbData.createdAt.seconds),
         name: dbData.name,
         phone: dbData.phone,
       });
     });
   } catch (error) {
     console.error('Fehler beim Abrufen der Daten: ', error);
-    console.log(db)
+    console.log(db);
   }
 });
+
+function convertTimestamp(unix_timestamp: number): string {
+  const date = new Date(unix_timestamp * 1000);
+  return date.toLocaleString('de-DE', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
